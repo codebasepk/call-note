bcfca893abdd72b332dff0dca3f0b4f6b2434f42
Husnain Kareem <husnain446@gmail.com>
2015-06-18 13:16:13 +0500

    save switch state on phone boot, row update method for database

---
+++ app/src/main/java/com/byteshaft/callnote/PhoneBootStateReader.java
@@ -0,0 +1,22 @@
+package com.byteshaft.callnote;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PhoneBootStateReader extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isServiceSettingEnabled(context)) {
+            context.startService(new Intent(context, OverlayService.class));
+        }
+    }
+
+    private boolean isServiceSettingEnabled(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getBoolean("enabled", false);
+    }
+}
